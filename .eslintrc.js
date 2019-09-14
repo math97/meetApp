6760af53924eda8878f31a88fe2760e69156edf3
math <math.albuquerque@outlook.com>
2019-09-14 09:49:52 -0300

    configurando prettier/eslint e editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "airbnb"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base','prettier'
+  ],
+  plugins:['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "class-method-use-this":"off",
+    "no-param-reassign":"off",
+    "camelcalse":"off",
+    "no-unused-vars":["error",{"argsIgnorePattern":"next"}],
+    "prettier/prettier":"error"
+  },
 };
